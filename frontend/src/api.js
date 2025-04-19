a03c953134fe641e34d3bc6d2093763ad34b37c1
SiahHau Daŋ <358707676@qq.com>
2025-04-20 06:50:43 +0800

    4

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000',
+  baseURL: import.meta.env.VITE_API_URL || 'http://119.27.184.159',
 });
 
 api.interceptors.request.use(
