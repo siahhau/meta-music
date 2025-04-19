8f500b34548168a32611b2d3dd72cdf4fe747c68
SiahHau Daŋ <358707676@qq.com>
2025-04-20 02:55:52 +0800

    Initial commit of meta-music project

---
+++ client/app/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Book Website</h1>
+      <div>
+        <input type='text' placeholder='Book Title...' />
+        <input type='number' placeholder='Release Date...' /> 
+        <button>Add Book</button>
+      </div>
+    </>
+  )
+}
+
+export default App
